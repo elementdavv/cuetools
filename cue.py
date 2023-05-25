9aae08ae5eb4586d4079a0964f12f37fa45ab922
elementdavv <vinctai@gmail.com>
2023-05-25 13:43:58 +0800

    parse folder for performer

--- cue.py
+++ cue.py
@@ -3,23 +3,24 @@
 '''
 desc:
   create cue file for music files in a directory
-  the index/performer/title are get from filename
+  the track/performer/title are got from filename
 
 usage:
-  python3 cue.py [-t title] [-p performer] [directory]
+  python3 cue.py [-a album] [-p performer] [-d directory]
 
 params:
-  use directory name as title if no title param
-  use "various artists" as performer if no performer param
-  use current director if no directory param
+  use current director if no directory provided
+  parse directory for album if no album provided
+  parse directory or use "various artists" for performer if no performer provided
+  parse filename for track or use sequencial
 
 music file formats supported:
   wav/flac/ape/m4a/dsf
 
 following formats supported:(blank between is optional)
-  index. performer - title
-  index. title -- performer
-  index. title
+  track. performer - title
+  track. title -- performer
+  track. title
   performer - title
   title -- performer
   title
@@ -44,26 +45,42 @@ class Cue:
     main class
     '''
 
-    def __init__(self, title, performer, directory):
-        self.title = title
-        self.performer = performer
-        self.directory = os.path.abspath(os.path.join(os.getcwd(), directory))
+    def __init__(self, album, performer, directory):
+        if directory is None or not os.path.isdir(directory):
+            directory = os.getcwd()
+        self.directory = directory
+
+        _, dirname = os.path.split(self.directory)
+        pa = dirname.split('-')
+
+        if album is None:
+            if len(pa) > 1:
+                self.album = pa[1].strip()
+            else:
+                self.album = dirname
+        else:
+            self.album = album
+
+        if performer is None:
+            if len(pa) > 1:
+                self.performer = pa[0].strip()
+            else:
+                self.performer = 'various artists'
+        else:
+            self.performer = performer
+
         self.track = 0
 
     def make_cue(self):
         '''
         main method
         '''
-        _, dirname = os.path.split(self.directory)
-        if self.title is None:
-            self.title = dirname
 
         cuefile = os.path.join(self.directory, CUEFILENAME)
         file_handle = open(cuefile, 'w')
 
-        file_handle.write('TITLE "' + self.title + '"\n')
-        if self.performer:
-            file_handle.write('PERFORMER "' + self.performer + '"\n')
+        file_handle.write('TITLE "' + self.album + '"\n')
+        file_handle.write('PERFORMER "' + self.performer + '"\n')
 
         files = os.listdir(self.directory)
         files.sort()
@@ -88,8 +105,10 @@ class Cue:
         file_name, _ = os.path.splitext(file_name)
         finfo = file_name.split('.', 1)
         if len(finfo) > 1:
-            track = finfo[0].strip()
-            file_name = finfo[1].strip()
+            track1 = finfo[0].strip()
+            if track1.isnumeric():
+                track = track1
+                file_name = finfo[1].strip()
 
         finfo = file_name.split('--', 1)
         if len(finfo) > 1:
@@ -105,7 +124,7 @@ class Cue:
 
         file_handle.write('  TRACK ' + track + ' AUDIO\n')
         file_handle.write('    TITLE "' + title + '"\n')
-        if performer:
+        if performer is not None:
             file_handle.write('    PERFORMER "' + performer + '"\n')
         file_handle.write('    INDEX 01 00:00:00\n')
 
@@ -120,12 +139,11 @@ class Cue:
 
 
 PARSER = argparse.ArgumentParser()
-PARSER.add_argument('--title', '-t', help='album title')
-PARSER.add_argument('--performer', '-p',
-                    help='song performer', default='varial artists')
-PARSER.add_argument('directory', help='target directory')
+PARSER.add_argument('--album', '-a')
+PARSER.add_argument('--performer', '-p')
+PARSER.add_argument('--directory', '-d')
 ARGS = PARSER.parse_args()
 
 if __name__ == '__main__':
-    CUE = Cue(ARGS.title, ARGS.performer, ARGS.directory)
+    CUE = Cue(ARGS.album, ARGS.performer, ARGS.directory)
     CUE.make_cue()
